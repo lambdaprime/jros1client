150d59a815bd9e02de68fb734dff33a424b3eee8
lambdaprime <id.blackmesa@gmail.com>
2019-12-25 19:58:59 -0800

    Parameterized tests for ConnectionHeaderWriter

---
+++ jrosclient.tests/src/test/java/id/jrosclient/tests/ConnectionHeaderSamples.java
@@ -0,0 +1,36 @@
+package id.jrosclient.tests;
+
+import id.jrosclient.ros.transport.ConnectionHeader;
+import id.xfunction.function.Unchecked;
+import id.xfunction.XUtils;
+
+public class ConnectionHeaderSamples {
+
+    public static class ConnectionHeaderSample {
+        private final String resource;
+        private final ConnectionHeader header;
+        
+        ConnectionHeaderSample(String resource, ConnectionHeader header) {
+            this.resource = resource;
+            this.header = header;
+        }
+    
+        String getResource() {
+            return resource;
+        }
+    
+        ConnectionHeader getHeader() {
+            return header;
+        }
+    }
+    
+    private static final String MESSAGE_DATA = "string data";
+
+    static final ConnectionHeaderSample HEADER = new ConnectionHeaderSample("/connection_header", new ConnectionHeader()
+            .withTopic("/topic")
+            .withCallerId("jrosclient")
+            .withType("std_msgs/String")
+            .withMessageDefinition(MESSAGE_DATA)
+            .withMd5Sum(Unchecked.runUnchecked(() -> XUtils.md5Sum(MESSAGE_DATA))));
+
+}
--- jrosclient.tests/src/test/java/id/jrosclient/tests/ConnectionHeaderWriterTests.java
+++ jrosclient.tests/src/test/java/id/jrosclient/tests/ConnectionHeaderWriterTests.java
@@ -6,31 +6,28 @@ import java.io.DataOutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import id.jrosclient.ros.transport.ConnectionHeader;
 import id.jrosclient.ros.transport.ConnectionHeaderWriter;
+import id.jrosclient.tests.ConnectionHeaderSamples.ConnectionHeaderSample;
 import id.jrosclient.tests.utils.OutputStreamCollector;
-import id.xfunction.XUtils;
 
 public class ConnectionHeaderWriterTests {
 
-    @Test
-    public void test_happy() throws Exception {
-        String topic = "topic";
-        String type = "std_msgs/String";
-        String messageDefinition = "string data";
-        var ch = new ConnectionHeader()
-                .withTopic("/" + topic)
-                .withCallerId("jrosclient")
-                .withType(type)
-                .withMessageDefinition(messageDefinition )
-                .withMd5Sum(XUtils.md5Sum(messageDefinition));
+    private static Stream<ConnectionHeaderSample> headerSamples() {
+        return Stream.of(ConnectionHeaderSamples.HEADER);
+    }
+
+    @ParameterizedTest
+    @MethodSource("headerSamples")
+    public void test_happy(ConnectionHeaderSample sample) throws Exception {
         OutputStreamCollector collector = new OutputStreamCollector();
         var dos = new DataOutputStream(collector);
-        new ConnectionHeaderWriter(dos).write(ch);
-        List<String> expected = TestUtils.readFileAsStream("/connection_header")
+        new ConnectionHeaderWriter(dos).write(sample.getHeader());
+        List<String> expected = TestUtils.readFileAsStream(sample.getResource())
             .map(l -> Arrays.asList(l.split(" ")))
             .flatMap(List::stream)
             .collect(Collectors.toList());
