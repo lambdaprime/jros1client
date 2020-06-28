400d400069127242a5584accd8c8634cd7521150
lambdaprime <id.blackmesa@gmail.com>
2020-06-27 21:24:54 -0700

    Adding comments

--- jrosclient/src/main/java/id/jrosclient/ros/api/MasterApi.java
+++ jrosclient/src/main/java/id/jrosclient/ros/api/MasterApi.java
@@ -4,11 +4,46 @@ import id.jrosclient.ros.responses.ListResponse;
 import id.jrosclient.ros.responses.StringResponse;
 import id.jrosclient.ros.responses.SystemStateResponse;
 
+/**
+ * @see <a href="http://wiki.ros.org/ROS/Master_API">ROS Maste API</a>
+ */
 public interface MasterApi {
 
+    /**
+     * Retrieve list representation of system state (i.e. publishers, subscribers,
+     * and services)
+     * 
+     * @param callerId ROS caller ID
+     */
     SystemStateResponse getSystemState(String callerId);
+    
+    /**
+     * Get the URI of the master
+     * 
+     * @param callerId ROS caller ID
+     * @return masterURI
+     */
     StringResponse getUri(String callerId);
-    StringResponse lookupService(String callerId, String service);
+        
+    /**
+     * Register the caller as a publisher the topic
+     * 
+     * @param callerId ROS caller ID 
+     * @param topic Fully-qualified name of topic
+     * @param topicType Message name
+     * @return List of current subscribers of topic
+     */
     ListResponse<String> registerPublisher(String callerId, String topic, String topicType);
+    
+    /**
+     * Subscribe the caller to the specified topic
+     * 
+     * @param callerId ROS caller ID 
+     * @param topic Fully-qualified name of topic.
+     * @param topicType Message name
+     * @return list of current publishers
+     */
     ListResponse<String> registerSubscriber(String callerId, String topic, String topicType);
+    
+    StringResponse lookupService(String callerId, String service);
 }
--- jrosclient/src/main/java/id/jrosclient/ros/api/NodeApi.java
+++ jrosclient/src/main/java/id/jrosclient/ros/api/NodeApi.java
@@ -5,7 +5,23 @@ import java.util.List;
 import id.jrosclient.ros.entities.Protocol;
 import id.jrosclient.ros.responses.ProtocolParamsResponse;
 
+/**
+ * @see <a href="http://wiki.ros.org/ROS/Slave_API">ROS Node API</a>
+ */
 public interface NodeApi {
 
+    /**
+     * Publisher node API method called by a subscriber node. This requests
+     * that source allocate a channel for communication. Subscriber provides
+     * a list of desired protocols for communication. Publisher returns the
+     * selected protocol along with any additional params required for establishing
+     * connection. For example, for a TCP/IP-based connection, the source node may
+     * return a port number of TCP/IP server. 
+     * 
+     * @param callerId ROS caller ID
+     * @param topic Topic name
+     * @param protocols List of desired protocols for communication in order of preference.
+     * @return
+     */
     ProtocolParamsResponse requestTopic(String callerId, String topic, List<Protocol> protocols);
 }
