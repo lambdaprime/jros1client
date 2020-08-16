7a4215e783dff2e45440b219aadd43c80b4d2a14
lambdaprime <id.blackmesa@gmail.com>
2020-08-15 21:12:45 -0700

    Method for transforming messages to stream of bytes

--- jrosclient/src/main/java/id/jrosmessages/MessageTransformer.java
+++ jrosclient/src/main/java/id/jrosmessages/MessageTransformer.java
@@ -1,18 +1,33 @@
 package id.jrosmessages;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
 import id.kineticstreamer.KineticStreamReader;
+import id.kineticstreamer.KineticStreamWriter;
 
 public class MessageTransformer {
 
     public <M extends Message> M transform(byte[] data, Class<M> clazz) {
         try {
             var dis = new DataInputStream(new ByteArrayInputStream(data));
-            KineticStreamReader ks = new KineticStreamReader(new RosDataInput(dis));
+            var ks = new KineticStreamReader(new RosDataInput(dis));
             Object obj = ks.read(clazz);
             return (M) obj;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public byte[] transform(Message message) {
+        try {
+            var bos = new ByteArrayOutputStream();
+            var dos = new DataOutputStream(bos);
+            var ks = new KineticStreamWriter(new RosDataOutput(dos));
+            ks.write(message);
+            return bos.toByteArray();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
