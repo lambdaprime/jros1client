e3d8387f4d44a2b3fbb613099bcf41797f430801
lambdaprime <intid@protonmail.com>
2021-03-26 03:39:51 +0000

    Adding missing headers

--- jrosclient.samples/src/main/java/id/jrosclient/samples/polygon/PolygonApp.java
+++ jrosclient.samples/src/main/java/id/jrosclient/samples/polygon/PolygonApp.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2020 jrosclient project
+ * 
+ * Website: https://github.com/lambdaprime/jrosclient
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Authors:
+ * - lambdaprime <id.blackmesa@gmail.com>
+ */
 package id.jrosclient.samples.polygon;
 
 import id.jrosclient.JRosClient;
@@ -9,7 +30,7 @@ import id.jrosmessages.std_msgs.HeaderMessage;
 public class PolygonApp {
 
     public static void main(String[] args) throws Exception {
-        var client = new JRosClient("http://ubuntu:11311/");
+        var client = new JRosClient("http://localhost:11311/");
         String topic = "/PolygonExample";
         var publisher = new TopicSubmissionPublisher<>(PolygonStampedMessage.class, topic);
         client.publish(publisher);
